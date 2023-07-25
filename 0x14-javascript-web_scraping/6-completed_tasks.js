c40dcc543f3d744a888316abd7d08413d1cd2626
root <ovedheo@gmail.com>
2023-07-25 15:05:44 -0700

    Tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, response, body) => {
+request(url, (err, res, body) => {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    tasks.forEach(task => {
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
-    });
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
+
+  const todos = JSON.parse(body);
+  const data = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed && data[todo.userId] === undefined) {
+      data[todo.userId] = 1;
+    } else if (todo.completed) {
+      data[todo.userId] += 1;
+    }
+  });
+
+  console.log(data);
 });
