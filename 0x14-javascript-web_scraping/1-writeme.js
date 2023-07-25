8036f7132a98d28e330aef7b3d3372b938f41b87
root <ovedheo@gmail.com>
2023-07-25 14:57:10 -0700

    Tasks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,23 +1,10 @@
 #!/usr/bin/node
-
-/* a Script that writes a string to a file */
 const fs = require('fs');
+const path = process.argv[2];
+const argms = process.argv[3];
 
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-const writeFileContent = (filePath, content) => {
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-      console.log('Error writing to file:', err);
-    } else {
-      console.log('file written successfully');
-    }
-  });
-};
-
-if (filePath && contentToWrite) {
-  writeFileContent(filePath, contentToWrite);
-} else {
-  console.error('please provide a file path');
-}
+fs.writeFile(path, argms, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
